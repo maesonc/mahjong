2701d81e7e76801952916506f99229b1c42e9d84
taijunjet <junjet.tai@swisslog.com>
2020-07-17 15:22:01 +0800

    Key_Class completed init, give_key, throw_key

--- Key_Class.py
+++ Key_Class.py
@@ -1,60 +1,59 @@
+import random
+
 class Key_Class:
 
+    NUMBER_OF_KEYS = 144
+
     available_keys = {}
+    thrown_keys = {}
+    key_dictionary= [
+        '1man', '2man', '3man', '4man', '5man', '6man', '7man', '8man', '9man',
+        
+        '1sok', '2sok', '3sok', '4sok', '5sok', '6sok', '7sok', '8sok', '9sok',
+        
+        '1tong', '2tong', '3tong', '4tong', '5tong', '6tong', '7tong', '8tong', '9tong',
+        
+        'dong', 'nan', 'sai', 'bok',
+        
+        'hongzhong', 'chengchoi', 'bakban',
+        
+        'fa1', 'fa2', 'fa3', 'fa4'
+        ]
 
     def __init__(self):
-        self.available_keys['1man'] = 4
-        self.available_keys['2man'] = 4
-        self.available_keys['3man'] = 4
-        self.available_keys['4man'] = 4
-        self.available_keys['5man'] = 4
-        self.available_keys['6man'] = 4
-        self.available_keys['7man'] = 4
-        self.available_keys['8man'] = 4
-        self.available_keys['9man'] = 4
+        for key_name in self.key_dictionary:
+            self.available_keys[key_name] = 4
 
-        self.available_keys['1sok'] = 4
-        self.available_keys['2sok'] = 4
-        self.available_keys['3sok'] = 4
-        self.available_keys['4sok'] = 4
-        self.available_keys['5sok'] = 4
-        self.available_keys['6sok'] = 4
-        self.available_keys['7sok'] = 4
-        self.available_keys['8sok'] = 4
-        self.available_keys['9sok'] = 4
-        
-        self.available_keys['1tong'] = 4
-        self.available_keys['2tong'] = 4
-        self.available_keys['3tong'] = 4
-        self.available_keys['4tong'] = 4
-        self.available_keys['5tong'] = 4
-        self.available_keys['6tong'] = 4
-        self.available_keys['7tong'] = 4
-        self.available_keys['8tong'] = 4
-        self.available_keys['9tong'] = 4
-
-        self.available_keys['dong'] = 4
-        self.available_keys['nan'] = 4
-        self.available_keys['si'] = 4
-        self.available_keys['bei'] = 4
-
-        self.available_keys['hongzhong'] = 4
-        self.available_keys['chengchoi'] = 4
-        self.available_keys['bakban'] = 4
-
-        self.available_keys['flower1'] = 2
-        self.available_keys['flower2'] = 2
-        self.available_keys['flower3'] = 2
-        self.available_keys['flower4'] = 2
+        self.available_keys['fa1'] = 2
+        self.available_keys['fa2'] = 2
+        self.available_keys['fa3'] = 2
+        self.available_keys['fa4'] = 2
 
         number_of_keys = 0
         for count in self.available_keys.values():
             number_of_keys += count
 
-        assert number_of_keys == 144, "Error! Initialized keys do not total to 144!"
+        assert number_of_keys == self.NUMBER_OF_KEYS, "Error! Initialized keys do not total to 144!"
         
     def give_key(self):
-        pass
+        if len(self.available_keys) < 1:
+            raise Exception("Error! Attempted to draw key from completed pile!")
+
+        random_key = random.choice(list(self.available_keys))
+
+        self.available_keys[random_key] -= 1
+        if self.available_keys[random_key] == 0:
+            del self.available_keys[random_key]
+
+        return random_key
 
     def throwaway_key(self, throw_key):
-        pass
+        assert throw_key in self.key_dictionary, "Error! Attempted to throw away non-existent key!"
+
+        if throw_key not in self.thrown_keys:
+            self.thrown_keys[throw_key] = 1
+        else:
+            assert self.thrown_keys[throw_key] < 4, "Error! Attempted to throw away key that's been thrown four times!"
+            self.thrown_keys[throw_key] += 1
+
+        
